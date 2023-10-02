d45851de7878b9f3432f976d7ad4cac0e7ce1c06
Pia <gayeongparkk@gmail.com>
2023-10-02 20:00:15 +0900

    monitoring!

--- ethers/src/main.rs
+++ ethers/src/main.rs
@@ -16,6 +16,14 @@ struct OutputProposed {
     l2_block_number: I256,
     l1_time_stamp: I256,
 }
+
+#[derive(EthEvent, Debug)]
+#[ethevent(abi = "numberTracker(uint256 num, string str)")]
+struct NumberTracker {
+    num: U256,
+    str: String,
+}
+
 abigen!(
     IPROXY,
     r#"[
@@ -24,15 +32,26 @@ abigen!(
 ]"#,
 );
 
+abigen!(
+    IUNISWAP,
+    r#"[
+    event numberTracker(uint256 num, string str)
+]"#,
+);
+
 #[tokio::main]
 async fn main() -> Result<()> {
     const HTTP_URL: &str = "";
     const OP_PROPOSER_ADDRESS: &str = "0xdfe97868233d1aa22e815a266982f2cf17685a27";
+    const UNISWAP_ADDRESS: &str = "0x3E0040916751DeEBF15B286cA0fB3B5D3722574E";
 
     let provider = Provider::<Http>::try_from(HTTP_URL)?;
     let client = Arc::new(provider);
     let address: Address = OP_PROPOSER_ADDRESS.parse()?;
-    let contract = IPROXY::new(address, client);
+    // let contract = IPROXY::new(address, client);
+
+    let uni_address: Address = UNISWAP_ADDRESS.parse()?;
+    let contract = IUNISWAP::new(uni_address, client);
 
     // if let Ok(version_info) = contract.version().call().await {
     //     println!("Version info is {version_info:?}");
@@ -48,13 +67,14 @@ async fn main() -> Result<()> {
 /// Note that all event bindings have been generated
 /// by abigen. Feel free to investigate the abigen expanded code to
 /// better understand types and functionalities.
-async fn listen_specific_events(contract: &IPROXY<Provider<Http>>) -> Result<()> {
-    let events = contract.event::<OutputProposed>().from_block(0);
-    let mut stream = events.stream().await?.take(1);
-
-    while let Some(Ok(f)) = stream.next().await {
-        println!("ApprovalFilter event: {f:?}");
+async fn listen_specific_events(contract: &IUNISWAP<Provider<Http>>) -> Result<()> {
+    let events = contract.event::<NumberTracker>().from_block(0);
+    let mut stream = events.stream().await?;
+    println!("ohayo2");
+    loop {
+        match stream.next().await {
+            Some(result) => println!("ohayo {result:?}"),
+            None => {}
+        }
     }
-
-    Ok(())
 }
